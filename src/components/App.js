ae102419cd3fd44aacb5933d876ac53f17b146c5
Goksel Ozgur <gokselozgur5@gmail.com>
2021-04-10 21:37:16 +0300

    end of project

--- src/components/App.js
+++ src/components/App.js
@@ -1,109 +1,177 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import SearchBar from './SearchBar';
 import axios from 'axios';
-
+import AddMovie from './AddMovie';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import EditMovie from './EditMovie';
 
 class App extends React.Component {
-    state = {
-        movies: [
 
-        ],
+    state = {
+        movies: [],
 
         searchQuery: ""
-
     }
 
+    /*     async componentDidMount() {
+            const baseURL = "http://localhost:3002/movies";
+            const response = await fetch(baseURL);
+            console.log(response)
+            const data = await response.json();
+            console.log(data)
+            this.setState({movies: data})
+        } */
+
     async componentDidMount() {
-        const response = await axios.get("http://localhost:3002/movies")
-        console.log(response);
+       this.getMovies();
+    }
+
+    async getMovies() {
+        const response = await axios.get("http://localhost:3002/movies");
+        //console.log(response);
         this.setState({ movies: response.data })
     }
 
-    /* deleteMovie = (movie) => {
-         const newMoviesList = this.state.movies.filter(
-             m => m.id !== movie.id
-         );*/
 
-    /*  this.setState ({
-          movies: newMoviesList
-      })
-
-*/
-    /*
-            this.setState(state=> ({
-                movies:newMoviesList
+    /*     deleteMovie = (movie) => {
+            const newMovieList = this.state.movies.filter(
+                m => m.id !== movie.id
+            );
+            this.setState(state => ({
+                movies: newMovieList
             }))
-    
-        } */
+        }  */
 
-    // FETCH METHOD
-    // deleteMovie = async (movie) => {
-    //     const baseURL = `http://localhost:3002/movies/${movie.id}`
+    // FETCH API
+    /*     deleteMovie =  async (movie) => {
+            const baseURL = `http://localhost:3002/movies/${movie.id}`
+            await fetch(baseURL, {
+                method: "DELETE"
+            })
+            const newMovieList = this.state.movies.filter(
+                m => m.id !== movie.id
+            );
+            this.setState(state => ({
+                movies: newMovieList
+            }))
+        }  */
 
-    //     await fetch(baseURL, {
-    //         method: "DELETE"
-    //     })
-    //     const newMoviesList = this.state.movies.filter(
-    //         m => m.id !== movie.id
-    //     );
-
-
-
-    /*  this.setState ({
-          movies: newMoviesList
-      })
-
-*/
-
-    //AXIOS METHOD DELETE
-
+    // AXIOS API
     deleteMovie = async (movie) => {
+
         axios.delete(`http://localhost:3002/movies/${movie.id}`)
-        const newMoviesList = this.state.movies.filter(
+        const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         );
-       
-
-    
-
         this.setState(state => ({
-        movies: newMoviesList
-    }))
+            movies: newMovieList
+        }))
+    }
+
+
+    searchMovie = (event) => {
+        //console.log(event.target.value)
+        this.setState({ searchQuery: event.target.value })
+    }
+
+    addMovie = async (movie) => {
+        await axios.post(`http://localhost:3002/movies/`, movie)
+        this.setState(state => ({
+            movies:state.movies.concat([movie])
+        }))
+        this.getMovies();
+    }
+
+    editMovie = async (id, updatedMovie) => {
+        await axios.put(`http://localhost:3002/movies/${id}`, updatedMovie)
+        this.getMovies();
+        
+    }
+
+    render() {
+
+        let filteredMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+            }
+        ).sort( (a,b) => {
+            return a.id < b.id ? 1 : a.id > b.id ? -1 : 0
+        });
+
+        return (
+
+            <Router >
+                 <div className="container">
+                     <Switch>
+                    
+                <Route path="/" exact render={() => (
+                    <React.Fragment>
+                    
+                     <div className="row">
+                         <div className="col-lg-12">
+                             <SearchBar searchMovieProp={this.searchMovie} />
+                         </div>
+                     </div>
+ 
+                     <MovieList
+                         movies={filteredMovies}
+                         deleteMovieProp={this.deleteMovie} />
+ 
+                
+                 </React.Fragment>
+                )}>
+
+                   
+               
+                </Route>
+               
+                <Route path="/add" exact render={({history}) => (
+                    <AddMovie 
+                    
+                    onAddMovie = {(movie) => {this.addMovie(movie)
+                        history.push("/")}
+                }
+                            
+                    />
+                 )}>
+
+                   
+               
+                 </Route>
+
+                 <Route path="/edit/:id" exact render={(props) => (
+                    <EditMovie 
+                    {...props}
+                    onEditMovie = {(id, movie) => {this.editMovie(id, movie)
+                        }
+                }
+                            
+                    />
+                 )}>
+
+                   
+               
+                 </Route>
+
+               
+
+                </Switch>
+                  
+                  
+                    
+              
+                </div>
+            </Router>
+
+
+
+
+        )
 
     }
 
-searchMovie = (event) => {
 
-    console.log(event.target.value)
-    this.setState({ searchQuery: event.target.value })
 }
-
-
-
-render(){
-
-    let filteredMovies = this.state.movies.filter(
-        (movie) => {
-            return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
-        }
-    )
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <SearchBar searchMovieProp={this.searchMovie} />
-                </div>
-            </div>
-            <MovieList
-                movies={filteredMovies}
-                deleteMovieProp={this.deleteMovie}
-            />
-        </div>
-    )
-}
-}
-
 
 export default App;
